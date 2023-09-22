8309abff11addfea9b4a0ab882479f4a4fcee329
Tyuiu.GrigorevKU@outlook.com <uhbujhmtdrjcnz@gmail.com>
2023-09-22 12:16:00 +0500

    Add project files.

---
+++ Tyuiu.GrigorevKU.Sprint0.TaskReview.V0.Test/DataServiceTest.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.GrigorevKU.Sprint0.TaskReview.V0.Lib;
+namespace Tyuiu.GrigorevKU.Sprint0.TaskReview.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckCalc()
+        {
+            Assert.AreEqual(DataService.Calc(1, 2, 3), 30);
+        }
+    }
+}
---
+++ Tyuiu.GrigorevKU.Sprint0.TaskReview.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.GrigorevKU.Sprint0.TaskReview.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.GrigorevKU.Sprint0.TaskReview.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0d626ca9-8536-4840-b373-eacd01ed4410")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
